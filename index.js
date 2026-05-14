0ac31324d170b8dfc0988e520483364fb864bbc6
tiku12-ops <timokuusistoster@gmail.com>
2026-05-14 15:23:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! It's year 2026</h1>')
+  res.send('<h1>Hello World! It\'s year 2026</h1>')
 })
 
 const PORT = 8080
